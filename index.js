dcc78a61d35fb25c7981c399d45075d3cc12ecbc
logbam <73276114+loicmabilat@users.noreply.github.com>
2021-02-25 10:04:10 +0100

    Add files via upload
    site en html our voir la forme

---
+++ index.js
@@ -0,0 +1,13 @@
+
+let button = document.querySelector('.abonnement');
+
+let ensemble  = document.querySelector('.abonnement');
+
+
+button.addEventListener("click",function(){
+  ensemble.classList.add('app')});
+fermer.addEventListener("click",function(){
+    ensemble.classList.remove('app')});
+
+document.querySelector('anim').style.transition
+= "ease-in-out 2s";
